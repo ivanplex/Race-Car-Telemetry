8a44ca7d4185c1e19a4a9ea28271acb4a2d1f45f
team4 <ivan@ivanplex.com>
2017-11-22 20:27:57 +0000

    Serial Reader

--- com/serialtest.py
+++ com/serialtest.py
@@ -1,60 +1,20 @@
 #! /usr/bin/python
 
-from xbee import XBee
 import serial
 
-"""
-serial_example.py
-By Paul Malmsten, 2010
-Demonstrates reading the low-order address bits from an XBee Series 1
-device over a serial port (USB) in API-mode.
-"""
+ser = serial.Serial('/dev/ttyUSB0', 9600)
+try:
+	ser.isOpen()
+	print("Serial is open")
+except:
+	print("Serial is closed")
+	exit()
 
-def main():
-    """
-    Sends an API AT command to read the lower-order address bits from 
-    an XBee Series 1 and looks for a response
-    """
-    try:
-        
-        # Open serial port
-        ser = serial.Serial('/dev/ttyUSB0', 9600)
-        
-        # Create XBee Series 1 object
-        xbee = XBee(ser)
-        
-        
-        # Send AT packet
-        xbee.send('at', frame_id='A', command='DH')
-        
-        # Wait for response
-        response = xbee.wait_read_frame()
-        print response
-        
-        # Send AT packet
-        xbee.send('at', frame_id='B', command='DL')
-        
-        # Wait for response
-        response = xbee.wait_read_frame()
-        print response
-        
-        # Send AT packet
-        xbee.send('at', frame_id='C', command='MY')
-        
-        # Wait for response
-        response = xbee.wait_read_frame()
-        print response
-        
-        # Send AT packet
-        xbee.send('at', frame_id='D', command='CE')
-        
-        # Wait for response
-        response = xbee.wait_read_frame()
-        print response
-    except KeyboardInterrupt:
-        pass
-    finally:
-        ser.close()
-    
-if __name__ == '__main__':
-main()
+if(ser.isOpen()):
+	try:
+		while(1):
+			print(ser.read())
+	except Exception:
+		print("error")
+else:
+	print("cannot open serial port")
